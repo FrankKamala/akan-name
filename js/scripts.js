7aec96bb76e4159e751eef1e10c111073d45ffe7
FrankKamala <frankkamala@gmail.com>
2019-10-25 20:45:25 +0300

    added logic to check empty input

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 var century,year,day,dayOfWeek,dayOfMonth;
 
 function getInput(){
+  century = parseInt(document.getElementById("century").value);
+  year=parseInt(document.getElementById("year").value);
+  month = parseInt(document.getElementById("month").value);
+  dayOfMonth = parseInt(document.getElementById("day").value);
+
+  if(century == ""){
+    alert("Input the correct gender");
+    return false;
+  }else if (year == ""){
+    alert("Input the correct year");
+    return false;
+  }else if (month == ""){
+    alert("Input the correct month");
+    return false;
+  }else if(dayOfMonth == ""){
+    alert("input the correct date");
+    return false;
+  }
 
 }
